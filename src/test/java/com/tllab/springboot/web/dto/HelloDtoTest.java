6c6c9f91f617942d76112bdba972e7ab135fbf63
yonghak <yonghak602@gmail.com>
2022-12-12 13:49:39 +0900

    코드 정리

--- src/test/java/com/tllab/springboot/web/dto/HelloDtoTest.java
+++ src/test/java/com/tllab/springboot/web/dto/HelloDtoTest.java
@@ -13,11 +13,11 @@ public class HelloDtoTest {
         int amount = 1000;
 
         // when
-        // @RequiredArgsConstructor : private final 로 선언됨 변수들을 인자로 하는 생성자 생성
+        // @RequiredArgsConstructor : private final 로 선언된 변수들을 인자로 하는 생성자 생성
         HelloDto helloDto = new HelloDto(name, amount);
 
         // then
-        // @Getter : private 으로 선언된 변수들에 대해 Getter 함수 생성
+        // @Getter : private 으로 선언된 변수들에 대해 getter 생성
         assertThat(helloDto.getName()).isEqualTo(name);
         assertThat(helloDto.getAmount()).isEqualTo(amount);
     }
