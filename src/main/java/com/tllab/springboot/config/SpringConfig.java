a8bf80520f79327d0a5118d19d8d81f2d7ab8679
yonghak <yonghak602@gmail.com>
2022-12-12 13:29:31 +0900

    @EnableJpaAuding 설정 위치 변경

---
+++ src/main/java/com/tllab/springboot/config/SpringConfig.java
@@ -0,0 +1,9 @@
+package com.tllab.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class SpringConfig {
+}
