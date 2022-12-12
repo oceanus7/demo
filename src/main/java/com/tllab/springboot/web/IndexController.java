9d09ae6fc1dd222e61ce72ccf638bf557e45f652
yonghak <yonghak602@gmail.com>
2022-12-13 08:05:10 +0900

    게시글 수정 화면

--- src/main/java/com/tllab/springboot/web/IndexController.java
+++ src/main/java/com/tllab/springboot/web/IndexController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 // Mustache 사용시 @RestController 이 아니라 @Controller 을 사용해야 한다.
 @RequiredArgsConstructor
@@ -26,6 +27,12 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        model.addAttribute("post", postsService.findById(id));
+        return "posts-update";
     }
 
 }
