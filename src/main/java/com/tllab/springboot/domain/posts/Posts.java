d686d56e5fc4d32211b7314b43eaa543d895ace6
yonghak <yonghak602@gmail.com>
2022-12-12 14:35:51 +0900

    코드 정리

--- src/main/java/com/tllab/springboot/domain/posts/Posts.java
+++ src/main/java/com/tllab/springboot/domain/posts/Posts.java
@@ -7,23 +7,31 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
+// @NoArgsConstructor : 디폴트 생성자 자동 추가
 @Getter
 @NoArgsConstructor
 @Entity
 public class Posts extends BaseTimeEntity {
 
+    // @Id : PK 지정
+    // GenerationType.IDENTITY : Auto Increment 지정
+    // Long 으로 선언할 경우 데이터 타입 -> BIGINT
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // 데이터 타입 -> VARCHAR(500)
     @Column(length = 500, nullable = false)
     private String title;
 
+    // 데이터 타입 -> TEXT
     @Column(columnDefinition = "TEXT", nullable = false)
     private String content;
 
+    // @Column 이 선언되지 않아도 @Entity 로 선언된 클래스의 모든 인스턴스 변수는 컬럼이 된다.
     private String author;
 
+    // @Builder : 빌더 패턴 클래스 생성, 생성자에 포함된 필드만 빌더에 포함
     @Builder
     public Posts(String title, String content, String author) {
         this.title = title;
