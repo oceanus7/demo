739ca8bc52c08197911d1203bd721c1e0061a750
yonghak <yonghak602@gmail.com>
2022-12-12 16:26:29 +0900

    코드 정리

--- src/main/java/com/tllab/springboot/config/SpringConfig.java
+++ src/main/java/com/tllab/springboot/config/SpringConfig.java
@@ -3,6 +3,9 @@ package com.tllab.springboot.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+// @EnableJpaAuditing : JPA Auditing 어노테이션 활성화
+// Application 클래스(메인 클래스)에 선언하면, @WebMvcTest 에서 오류가 발생하므로 별도의 Configuration Bean 를 만들어서 선언한다.
+
 @EnableJpaAuditing
 @Configuration
 public class SpringConfig {
