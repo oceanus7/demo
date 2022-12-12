cd1175587327dff005a6bcdaaa013650fd38868f
yonghak <yonghak602@gmail.com>
2022-12-12 16:15:35 +0900

    코드 정리

--- src/main/java/com/tllab/springboot/service/posts/PostsService.java
+++ src/main/java/com/tllab/springboot/service/posts/PostsService.java
@@ -24,7 +24,12 @@ public class PostsService {
     public Long update(Long id, PostsUpdateDto posts) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        // 여기서 entity 를 별도로 save 하지 않아도 변경 사항이 DB 에 적용된다.
+        // JPA 에서 지원하는 기능으로, 트랜잭션 안에서 DB 에서 데이터를 가져오면 영속성 컨텍스트가 유지되며,
+        // 트랜잭션이 끝나는 시점에 변경사항을 체크(더티 체킹)해서 자동으로 적용시킨다.
         entity.update(posts.getTitle(), posts.getContent());
+
         return id;
     }
 
