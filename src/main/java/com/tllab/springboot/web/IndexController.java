e7a60935fd7b53789faa65d6b5714e34b0da7e70
yonghak <yonghak602@gmail.com>
2022-12-12 22:05:10 +0900

    개발 참고 사항 추가

--- src/main/java/com/tllab/springboot/web/IndexController.java
+++ src/main/java/com/tllab/springboot/web/IndexController.java
@@ -3,8 +3,14 @@ package com.tllab.springboot.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+// Mustache 사용시 @RestController 이 아니라 @Controller 을 사용해야 한다.
+
 @Controller
 public class IndexController {
+
+    // 스프링부트 2.7.x 에서 Mustache 사용 시 한글이 깨지는 문제가 있다.
+    // application.properties 에 다음을 추가한다.
+    // server.servlet.encoding.force-response=true
 
     @GetMapping("/")
     public String index() {
