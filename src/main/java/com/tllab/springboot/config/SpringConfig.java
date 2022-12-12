8b692d08b47a7626c46f0248db89ea60cd594eab
yonghak <yonghak602@gmail.com>
2022-12-12 16:42:52 +0900

    코드 정리

--- src/main/java/com/tllab/springboot/config/SpringConfig.java
+++ src/main/java/com/tllab/springboot/config/SpringConfig.java
@@ -4,7 +4,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 // @EnableJpaAuditing : JPA Auditing 어노테이션 활성화
-// Application 클래스(메인 클래스)에 선언하면, @WebMvcTest 에서 오류가 발생하므로 별도의 Configuration Bean 를 만들어서 선언한다.
+
+// @EnableJpaAuditing 을 @SpringBootApplication 이 선언된 Application(메인 클래스)에 선언할 경우,
+// @WebMvcTest 테스트 실행 시 @SpringBootApplication 을 스캔하면서 @EnableJpaAuditing 도 스캔하게 되는데, 이 때 오류가 발생한다.
+
+// @EnableJpaAuditing 을 사용하기 위해서는 최소 1개 이상의 Entity 가 필요한데, @WebMvcTest 테스트 실행 시에는 Entity 가
+// 존재하지 않기 때문에 발생하는 오류이며, @EnableJpaAuditing 을 아래와 같이 별도의 Configuration Bean 에 선언하면 된다.
 
 @EnableJpaAuditing
 @Configuration
