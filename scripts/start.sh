ebf1857ccb15eb674c63fe3ffd6fa5d0ff1f5c43
yonghak <yonghak602@gmail.com>
2022-12-23 10:38:48 +0900

    무중단 배포를 위한 스크립트 추가

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,11 +8,13 @@ REPOSITORY=/home/ec2-user/app/step3
 PROJECT_NAME=demo
 
 echo "> Build 파일 복사"
+
 echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 새 애플리케이션 배포"
+
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
