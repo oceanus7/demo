b8caa4354881c296f54ed7e340597edeae3974ba
yonghak <yonghak602@gmail.com>
2022-12-25 23:31:58 +0900

    DataSource Configuration Bean 등록

---
+++ src/main/java/com/tllab/demo/config/DatabaseConfig.java
@@ -0,0 +1,25 @@
+package com.tllab.demo.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.hikari")
+    public HikariConfig hikariConfig() {
+        return new HikariConfig();
+    }
+
+    @Bean
+    public DataSource dataSource(HikariConfig hikariConfig) throws Exception {
+        return new HikariDataSource(hikariConfig);
+    }
+
+}
